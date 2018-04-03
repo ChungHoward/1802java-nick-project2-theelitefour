e6bf693f7df5f112d170013503de8fae0966c909
Howard Chung <howardchung@temple.edu>
2018-04-03 09:17:53 -0400

    commit before making rename changes

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -12,7 +12,7 @@ import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { UpgradeComponent } from './upgrade/upgrade.component';
 
-const routes: Routes =[
+const routes: Routes = [
     { path: 'dashboard',      component: DashboardComponent },
     { path: 'user-profile',   component: UserProfileComponent },
     { path: 'table-list',     component: TableListComponent },
