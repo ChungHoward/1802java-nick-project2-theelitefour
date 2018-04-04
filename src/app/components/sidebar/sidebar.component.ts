9cfa84df1c4e2c6c050fe18417d24b97eef84fac
Howard Chung <howardchung@temple.edu>
2018-04-04 01:13:05 -0400

    Remind me to fork

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -8,14 +8,14 @@ declare interface RouteInfo {
     class: string;
 }
 export const ROUTES: RouteInfo[] = [
-    { path: 'dashboard', title: 'Teambuilder',  icon: 'view_quilt', class: '' }, // Testing icon change
+    { path: 'teambuilder', title: 'Teambuilder',  icon: 'view_quilt', class: '' },
     { path: 'register', title: 'Pokemon Box',  icon: 'grid_on', class: '' },
     { path: 'table-list', title: 'Core Coverage',  icon: 'repeat_one', class: '' },
     { path: 'typography', title: 'Defensive Coverage',  icon: 'view_list', class: '' },
     { path: 'icons', title: 'Offensive Coverage',  icon: 'view_stream', class: '' },
     { path: 'maps', title: 'Offense vs Defense',  icon: 'poll', class: '' },
-    { path: 'notifications', title: 'Notifications',  icon: 'notifications', class: '' }, // we don't need notifications
-    { path: 'upgrade', title: 'Upgrade to PRO',  icon: 'unarchive', class: 'active-pro' },
+    { path: 'notifications', title: 'Notifications',  icon: 'notifications', class: '' }, // TODO: remove notifications
+    // { path: 'upgrade', title: 'Upgrade to PRO',  icon: 'unarchive', class: 'active-pro' },
 ]; // leaving these here in case we add more features
 
 @Component({
