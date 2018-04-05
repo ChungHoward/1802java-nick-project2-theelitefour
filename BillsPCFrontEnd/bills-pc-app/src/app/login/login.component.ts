0a984d3b2898af5a611ddf942382e5c73d59e9e6
Robertson-Brian <kd7uns@gmail.com>
2018-04-05 10:52:43 -0400

    adding login component

---
+++ BillsPCFrontEnd/bills-pc-app/src/app/login/login.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username: string;
+  email: string;
+  password: string;
+  cpassword: string;
+
+  submitted: boolean;
+  passwordMatch: boolean;
+
+  constructor() {
+    this.submitted = false;
+    this.passwordMatch = false;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    const xhr = new XMLHttpRequest();
+    // receive reply
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const reply = JSON.parse(xhr.responseText);
+        alert(reply);
+
+        document.getElementById('notification').innerHTML = reply;
+      }
+    }
+    // send request to /login with fields, username, password, email
+    xhr.open('POST', '/login', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send('username=' + this.username + '&password=' + this.password + '&email=' + this.email);
+  }
+
+  matchPassword() {
+    if (this.password === this.cpassword) {
+      this.passwordMatch = true;
+    } else {
+      this.passwordMatch = false;
+    }
+  }
+
+  ngOnInit() { }
+
+}
+
+
