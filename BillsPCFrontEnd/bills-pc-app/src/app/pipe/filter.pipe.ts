b5f50d9498679b806ca65398711790dc190d67e7
Howard Chung <howardchung@temple.edu>
2018-04-05 12:48:04 -0400

    fixed searching!

--- BillsPCFrontEnd/bills-pc-app/src/app/pipe/filter.pipe.ts
+++ BillsPCFrontEnd/bills-pc-app/src/app/pipe/filter.pipe.ts
@@ -1,22 +1,54 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Pokemon } from '../pokemon';
 
 @Pipe({
-    name: 'filter'
+  name: 'pkmnFilter'
 })
 export class Filter implements PipeTransform {
 
-    transform(stringList: string[], searchText: string): string[] {
+  transform(pkmnList: Pokemon[], searchText: string): Pokemon[] {
 
-        if (!stringList) {
-            return [];
-        }
-
-        searchText = searchText.toLowerCase();
-
-        return stringList.filter( movie => {
-            let search: boolean;
-            search = movie.toLowerCase().includes(searchText);
-            return search;
-        });
+    if (!pkmnList) {
+      return [];
     }
+
+    if (!searchText) {
+      return pkmnList;
+    }
+
+    searchText = searchText.toLowerCase();
+
+    const newList = new Array<Pokemon>();
+
+    for (let i = 0; i < pkmnList.length; i++) {
+      const pkmn = pkmnList[i];
+      if (pkmn.name.toLowerCase().includes(searchText)) {
+        newList.push(pkmn);
+      }
+      console.log(pkmn.name + ' ' + searchText + ' ' + pkmn.name.toLowerCase().includes(searchText));
+    }
+
+    return newList;
+    /*
+
+    pkmnList.forEach(pkmn => {
+      console.log(pkmn.name + ' ' + searchText + ' ' + pkmn.name.toLowerCase().includes(searchText));
+      if (pkmn.name.includes(searchText)) {
+        console.log(' ' + pkmn.name);
+        newList.push(pkmn);
+      }
+    });
+    for (const pkmn of pkmnList) {
+      if (pkmn.name.includes(searchText)) {
+        newList.push(pkmn);
+      }
+    }
+
+    return pkmnList.filter(pkmn => {
+      let search: boolean;
+      search = pkmn.name.toLowerCase().includes(searchText);
+      return search;
+    });
+    */
+  }
 }
--- BillsPCFrontEnd/bills-pc-app/src/app/teambuilder/teambuilder.component.ts
+++ BillsPCFrontEnd/bills-pc-app/src/app/teambuilder/teambuilder.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { Filter } from 'app/pipe/filter.pipe';
 import { Pokemon } from 'app/pokemon';
+import { Filter } from 'app/pipe/filter.pipe';
 import * as Chartist from 'chartist';
 
 @Component({
@@ -54,7 +54,6 @@ export class TeambuilderComponent implements OnInit {
 
   /* These variables are for the Detailed Pokemon View/Search */
   questionSprite: string;
-  myStyle: any;
   searchInput: string;
 
   constructor() {
@@ -76,37 +75,50 @@ export class TeambuilderComponent implements OnInit {
     this.ghost = 'assets/img/types/ghost.png';
     this.dragon = 'assets/img/types/dragon.png';
 
-    /* Display your team on page load */
+    /* Making my team */
     this.pkmn1 = new Pokemon();
     this.pkmn2 = new Pokemon();
     this.pkmn3 = new Pokemon();
     this.pkmn4 = new Pokemon();
     this.pkmn5 = new Pokemon();
     this.pkmn6 = new Pokemon();
+
     this.pkmn1.name = 'Jolteon';
     this.pkmn1.sprite_url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/135.png';
     this.pkmn1.types = [this.electric, this.none];
     this.pkmn1.moveset = ['Thunderbolt', 'Pin Missile', 'Double Kick', 'Thunder Wave'];
+    this.pkmn1.stats = [65, 65, 60, 110, 110, 130];
+
     this.pkmn2.name = 'Exeggutor';
     this.pkmn2.sprite_url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/103.png';
     this.pkmn2.types = [this.grass, this.psychic];
     this.pkmn2.moveset = ['Sleep Powder', 'Reflect', 'Psychic', 'Explosion'];
+    this.pkmn2.stats = [95, 95, 85, 125, 125, 55];
+
     this.pkmn3.name = 'Moltres';
     this.pkmn3.sprite_url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/146.png';
     this.pkmn3.types = [this.fire, this.flying];
     this.pkmn3.moveset = ['Agility', 'Fire Spin', 'Fire Blast', 'Hyper Beam'];
+    this.pkmn3.stats = [90, 100, 90, 125, 125, 90];
+
     this.pkmn4.name = 'Slowbro';
     this.pkmn4.sprite_url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/80.png';
     this.pkmn4.types = [this.water, this.psychic];
     this.pkmn4.moveset = ['Amnesia', 'Surf', 'Psychic', 'Reflect'];
+    this.pkmn4.stats = [65, 75, 110, 80, 80, 30];
+
     this.pkmn5.name = 'Chansey';
     this.pkmn5.sprite_url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/113.png';
     this.pkmn5.types = [this.normal, this.none];
     this.pkmn5.moveset = ['Thunderbolt', 'Ice Beam', 'Counter', 'Softboiled'];
+    this.pkmn5.stats = [250, 5, 5, 105, 105, 50];
+
     this.pkmn6.name = 'Gengar';
     this.pkmn6.sprite_url = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/94.png';
     this.pkmn6.types = [this.ghost, this.poison];
     this.pkmn6.moveset = ['Thunderbolt', 'Mega Drain', 'Confuse Ray', 'Explosion'];
+    this.pkmn6.stats = [60, 65, 60, 130, 130, 110];
+
     this.favTeam = new Array<Pokemon>();
     this.favTeam.push(this.pkmn1);
     this.favTeam.push(this.pkmn2);
@@ -125,6 +137,8 @@ export class TeambuilderComponent implements OnInit {
   toggleCollapse() {
     this.expandOrCollapse = !this.expandOrCollapse;
   }
+
+  /* The following methods are for the charts */
 
   startAnimationForLineChart(chart) {
     let seq: any, delays: any, durations: any;
