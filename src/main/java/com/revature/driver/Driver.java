ecfba95fee412e6125a7d5f9eabe27d69facac5f
Kent Lathrop <jlathrop55@gmail.com>
2018-04-04 22:34:07 -0400

    Added Spring Annotations to hibernate objects

--- src/main/java/com/revature/driver/Driver.java
+++ src/main/java/com/revature/driver/Driver.java
@@ -4,7 +4,11 @@ package com.revature.driver;
 
 import org.hibernate.Session;
 import org.hibernate.Transaction;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.revature.domains.PokemonSet;
+import com.revature.domains.Team;
 import com.revature.domains.Trainer;
 import com.revature.util.HibernateUtil;
 
@@ -12,7 +16,7 @@ import com.revature.util.HibernateUtil;
 public class Driver {
 
 	public static void main(String[] args) {
-		Session sess = HibernateUtil.getSession();
+/*		Session sess = HibernateUtil.getSession();
 		Transaction t = sess.beginTransaction();
 		
 		Trainer tr = new Trainer(0,"test","password","default");
@@ -27,10 +31,40 @@ public class Driver {
 		
 		tr = (Trainer) sess.get(Trainer.class,200);
 		
-		System.out.println("first trainer: "+tr.getName());
+		System.out.println("first trainer: "+tr.getName());*/
+		
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		
+		Trainer tr = ac.getBean("trainer", Trainer.class);
+		tr.setName("Brock8");
+		tr.setPassword("password");
+		
+		PokemonSet ps1 = ac.getBean("pokemonSet", PokemonSet.class);
+		
+		ps1.setTrainer(tr);
 		
 		
-
+		Team team = ac.getBean("team", Team.class);
+		System.out.println(team);
+		team.setSet1(ps1);
+		
+		
+		System.out.println(team);
+		
+		Session sess = HibernateUtil.getSession();
+		Transaction t = sess.beginTransaction();
+		
+		int trainerId = (int) sess.save(tr);
+		int setId;
+		setId= (int) sess.save(ps1);
+		
+		int teamId = (int) sess.save(team);
+		System.out.println("Trainer Id :"+trainerId);
+		System.out.println("Set Id:"+setId);
+		System.out.println("Team Id:"+teamId);
+		t.commit();
+		sess.close();
+		System.out.println(team);
 	}
 
 }
