0cec150c233ef1ddde40da34835ed87bc248a88c
Howard Chung <howardchung@temple.edu>
2018-04-06 18:42:23 -0400

    some styling on pokemonbox

--- BillsPCFrontEnd/bills-pc-app/src/app/pokemonbox/pokemonbox.component.ts
+++ BillsPCFrontEnd/bills-pc-app/src/app/pokemonbox/pokemonbox.component.ts
@@ -17,7 +17,62 @@ export class PokemonBoxComponent implements OnInit {
   // Contains the image for every type and damage class
   types: TypeService;
 
-  constructor() { }
+  /* These variables are for the Detailed Pokemon View/Search */
+  questionSprite: string; // image for when no pokemon is selected. no, it's not missingno
+  pkmnTableColNames: Array<string>;
+  colSortIcons: Array<string>;
+  sortBy: string;
+  ascending: boolean;
+  searchInput: string;
+
+  constructor() {
+    // Assigns the value of types to their respective image
+    this.types = new TypeService();
+
+    /* Assign my favTeam using teamService */
+    this.teamService = new TeamService();
+    this.favTeam = this.teamService.favTeam;
+
+    // this can be used as a placeholder image before searching for a pokemon
+    this.questionSprite = 'assets/img/question.png';
+
+    this.pkmnTableColNames = ['name', 'type', 'hp', 'atk', 'def', 'satk', 'sdef', 'spe'];
+    this.colSortIcons = [ // The icon underneath each pkmnTableColNames
+      'swap_vert', 'swap_vert', 'swap_vert', 'swap_vert',
+      'swap_vert', 'swap_vert', 'swap_vert', 'swap_vert'
+    ];
+    this.sortBy = 'name';
+    this.ascending = true;
+
+  }
+
+  /**
+   * Switches all other column icons to swap_vert, then toggles the clicked icon,
+   * then changes the sorting strategy
+   * @param i the column index to sort
+   */
+  toggleSort(i: number) {
+    if (this.colSortIcons[i] === 'swap_vert') {
+      for (let k = 0; k < this.colSortIcons.length; k++) {
+        this.colSortIcons[k] = 'swap_vert';
+      }
+      this.colSortIcons[i] = 'arrow_drop_down';
+      this.ascending = false;
+    } else if (this.colSortIcons[i] === 'arrow_drop_down') {
+      for (let k = 0; k < this.colSortIcons.length; k++) {
+        this.colSortIcons[k] = 'swap_vert';
+      }
+      this.colSortIcons[i] = 'arrow_drop_up';
+      this.ascending = true;
+    } else if (this.colSortIcons[i] === 'arrow_drop_up') {
+      for (let k = 0; k < this.colSortIcons.length; k++) {
+        this.colSortIcons[k] = 'swap_vert';
+      }
+      this.colSortIcons[i] = 'arrow_drop_down';
+      this.ascending = false;
+    }
+    this.sortBy = this.pkmnTableColNames[i];
+  }
 
   ngOnInit() {
   }
