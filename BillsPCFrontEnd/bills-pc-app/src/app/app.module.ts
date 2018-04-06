7e0ec8c8938eef5e4cba23ef6b40c626a7ba950a
Howard Chung <howardchung@temple.edu>
2018-04-06 01:33:30 -0400

    rearranged stuff in app.module & app.routing in order of completion

--- BillsPCFrontEnd/bills-pc-app/src/app/app.module.ts
+++ BillsPCFrontEnd/bills-pc-app/src/app/app.module.ts
@@ -12,23 +12,23 @@ import { AppComponent } from './app.component';
 import { Filter } from './pipe/filter.pipe';
 import { Sort } from 'app/pipe/sort.pipe';
 
-import { TeambuilderComponent } from './teambuilder/teambuilder.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { TeambuilderComponent } from './teambuilder/teambuilder.component';
+import { PokemonBoxComponent } from './pokemon-box/pokemon-box.component';
 import { TypographyComponent } from './typography/typography.component';
 import { IconsComponent } from './icons/icons.component';
 import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { UpgradeComponent } from './upgrade/upgrade.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
-import { PokemonBoxComponent } from './pokemon-box/pokemon-box.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TeambuilderComponent,
     RegisterComponent,
     LoginComponent,
+    TeambuilderComponent,
     PokemonBoxComponent,
     TypographyComponent,
     IconsComponent,
--- BillsPCFrontEnd/bills-pc-app/src/app/app.routing.ts
+++ BillsPCFrontEnd/bills-pc-app/src/app/app.routing.ts
@@ -3,21 +3,21 @@ import { CommonModule, } from '@angular/common';
 import { BrowserModule } from '@angular/platform-browser';
 import { Routes, RouterModule } from '@angular/router';
 
-import { TeambuilderComponent } from './teambuilder/teambuilder.component';
 import { RegisterComponent } from './register/register.component';
 import { LoginComponent } from './login/login.component';
+import { TeambuilderComponent } from './teambuilder/teambuilder.component';
+import { PokemonBoxComponent } from './pokemon-box/pokemon-box.component';
 import { TypographyComponent } from './typography/typography.component';
 import { IconsComponent } from './icons/icons.component';
 import { MapsComponent } from './maps/maps.component';
 import { NotificationsComponent } from './notifications/notifications.component';
 import { UpgradeComponent } from './upgrade/upgrade.component';
 import { PokemonComponent } from './pokemon/pokemon.component';
-import { PokemonBoxComponent } from './pokemon-box/pokemon-box.component';
 
 const routes: Routes = [
-  { path: 'teambuilder', component: TeambuilderComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'teambuilder', component: TeambuilderComponent },
   { path: 'pokemon-box', component: PokemonBoxComponent },
   { path: 'typography', component: TypographyComponent },
   { path: 'icons', component: IconsComponent },
