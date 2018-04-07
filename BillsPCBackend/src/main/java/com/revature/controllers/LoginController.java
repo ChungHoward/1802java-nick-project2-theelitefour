77bbd8db71041698576d6ec7ce755522766679cc
Robertson-Brian <kd7uns@gmail.com>
2018-04-07 19:29:00 -0400

    please work

---
+++ BillsPCBackend/src/main/java/com/revature/controllers/LoginController.java
@@ -0,0 +1,22 @@
+package com.revature.controllers;
+
+import com.revature.services.LoginService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController
+{
+    @RequestMapping(method = RequestMethod.POST, value = "/login")
+    public void login(@RequestParam String username, String password, String email)
+    {
+        System.out.println("hello called from LoginController " + username + " " + password);
+
+        LoginService.login(username, password);
+
+        System.out.println("hello called from LoginController " + username + " " + password);
+        return;
+    }
+}
---
+++ BillsPCFrontEnd/bills-pc-app/src/app/login/login.component.ts
@@ -0,0 +1,50 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  username: string;
+  password: string;
+
+  submitted: boolean;
+
+  constructor() {
+    this.submitted = false;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    const xhr = new XMLHttpRequest();
+    // receive reply
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        const reply = JSON.parse(xhr.responseText);
+        alert(reply);
+
+        document.getElementById('notification').innerHTML = reply;
+      }
+    }
+    // send request to /login with fields, username, password, email
+    xhr.open('POST', 'http://localhost:8080/login', true);
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send("username=" + this.username + "&password=" + this.password);
+
+    //xhr.send('username=' + this.username + 'password=' + this.password);
+  }
+
+  ngOnInit() { }
+
+}
+
+
+
+
+
+
+
+
