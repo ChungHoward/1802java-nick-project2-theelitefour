bc7467acd67cdf52ccf99394bc6c0ced97a61b9c
Howard Chung <howardchung@temple.edu>
2018-04-10 10:41:37 -0400

    one last fix and it should be ok

--- BillsPCFrontEnd/bills-pc-app/src/app/services/register.service.ts
+++ BillsPCFrontEnd/bills-pc-app/src/app/services/register.service.ts
@@ -7,11 +7,7 @@ export class RegisterService {
 
   constructor(private http: HttpClient) { }
 
-<<<<<<< HEAD
   register(username: string, password: string, email: string): Observable<boolean> {
-=======
-  register(username: string, password: string, email: string): Observable<boolean>{
->>>>>>> staging
     const body = new HttpParams().set('username', username).set('password', password).set('email', email);
     return this.http.post<boolean>('register', body);
   }
