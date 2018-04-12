73163f38021988cf28d154bbbdaaefb82bb3eda4
Kent Lathrop <jlathrop55@gmail.com>
2018-04-11 21:35:24 -0400

    Made some edits to the angular tests

--- BillsPCFrontEnd/bills-pc-app/src/app/featured/featured.component.spec.ts
+++ BillsPCFrontEnd/bills-pc-app/src/app/featured/featured.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PokemonComponent } from './featured.component';
+import { FeaturedComponent } from './featured.component';
 
-describe('PokemonComponent', () => {
-  let component: PokemonComponent;
-  let fixture: ComponentFixture<PokemonComponent>;
+describe('FeaturedComponent', () => {
+  let component: FeaturedComponent;
+  let fixture: ComponentFixture<FeaturedComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PokemonComponent ]
+      declarations: [ FeaturedComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PokemonComponent);
+    fixture = TestBed.createComponent(FeaturedComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
